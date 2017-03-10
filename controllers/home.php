05ca8a6d05b41d94ebd028451f47cbc3eb264ca4
AMGitsKriss <sp4m@k-jessop.co.uk>
2017-03-10 00:20:27 +0000

    CSS & Minecraft playerlist refactoring

--- controllers/home.php
+++ controllers/home.php
@@ -1,9 +1,50 @@
 <?php
 	include_once("views/header.php");
 
-	$output .= "<div class=container>".file_get_contents("../adsense_leaderboard.html")."</div>";
-
 	# $output .= "<ul><li><a href='/map'>Minecraft Server Map</a></li><li><a href='/swn'>Stars Without Number Wiki</a></li>\n<li><a href='http://www.d20pfsrd.com/'>Pathfinder Rules (Wiki)</a></li>\n</ul>\n<p>";
+	use xPaw\MinecraftQuery;
+	use xPaw\MinecraftQueryException;
+	
+	function getUserList(){
+		
+		
+		require('lib/MinecraftQuery.php');
+		require('lib/MinecraftQueryException.php');
 
-	include_once("views/players.php");
+		$Query = new MinecraftQuery();
+
+		try{
+			$Query->Connect( "localhost", 25565, 1 );
+		}
+		catch( MinecraftQueryException $e ){
+			$Exception = $e;
+		}
+
+		$result = Array();
+		if( ( $Players = $Query->GetPlayers( ) ) !== false ){
+			foreach( $Players as $Player ){
+				array_push($result, [htmlspecialchars( $Player ), "lib/MinecraftFace.php?u=$Player&s=30"]);
+			}
+		}
+		elseif(!$Query->GetInfo()){
+			$result = [["<div style='color:red; font-weight:bold; text-align:center;'>SERVER OFFLINE</div>", ""]];
+		}
+		else{
+			$result = [["<div style='text-align:center; width:100%;'>Nobody's Online ;(</div>",""]];
+		}
+		return $result;
+	}
+
+	# Sidebar
+	$online_players = getUserList();
+	include_once("views/sidebar.php");
+
+	#Adsense
+	$output .= "<div id='mainsection'><div class=container>".file_get_contents("views/templates/adsense_leaderboard.html")."</div>";
+
+	$output .= "<div class=container><img src='images/0f00e3e818b461fb559a78f48ccbe285.gif'/>
+	<p>Ross says \"Hi.\"</p></div>";
+
+	#Bulk of page
+	$output .= "\n</div>";
 ?>
---
+++ views/sidebar.php
@@ -0,0 +1,5 @@
+<?php
+	$output .= "\n<div id=sidebar>";
+	include_once("views/players.php");
+	$output .= "\n</div>";
+?>
