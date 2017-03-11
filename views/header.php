510bbb046bc341da9926c32f2c98476920fdc48e
AMGitsKriss <sp4m@k-jessop.co.uk>
2017-03-11 17:42:32 +0000

    Added Repo link to header & page title.

--- views/header.php
+++ views/header.php
@@ -8,10 +8,11 @@
 	$links = [["Home", $host, '<i class="fa fa-home" aria-hidden="true"></i>'],
 		["Minecraft Map", $host."/map", '<i class="fa fa-globe" aria-hidden="true"></i>'],
 		["Pathfinder Wiki", "http://www.d20pfsrd.com/' target='_blank", '<i class="fa fa-bookmark" aria-hidden="true"></i>']];
-	$controls = [["", "#", '<i class="fa fa-sign-in" aria-hidden="true"></i>']];
+	$controls = [["", "#", '<i class="fa fa-sign-in" aria-hidden="true"></i>'],
+		["Repo", "https://github.com/AMGitsKriss/Orion-Dashboard' target='_blank", '<i class="fa fa-github" aria-hidden="true"></i>']];
 
 	# HTML Header
-	$output = "<!DOCTYPE html>\n<html>\n<head>\n<title>Welcome to Orion</title>\n
+	$output = "<!DOCTYPE html>\n<html>\n<head>\n<title>$site_title</title>\n
 	<meta property='og:image' content='http://qvvz.uk/images/0f00e3e818b461fb559a78f48ccbe285.gif' />
 	<script src='https://use.fontawesome.com/15e142434c.js'></script>
 	<link rel='stylesheet' type='text/css' href='css/default.css'>\n</head><body>";
