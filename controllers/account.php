b41b50591cdbf91eb9e3a8a9c499630d6d0aff94
AMGitsKriss <sp4m@k-jessop.co.uk>
2017-03-13 20:23:27 +0000

    Separated account controller into view & added account icon

--- controllers/account.php
+++ controllers/account.php
@@ -21,38 +21,8 @@
 
 	$userData = $userCheck->getUser($_COOKIE["orion_user_session"]);
 
-	if($userData){
-		$output .= "<p>$userData->username</p>";
-		$output .= "<p>$userData->email</p>";
-		$output .= "<p>$userData->mc_username</p>";
-		$output .= "<p>$userData->mc_avatar</p>";
-	}
-	else{
-		$output .= "<p class=error>Something weent wrong getting your account details.</p>";
-	}
-
 	$userMapShortcut = $database->query("selectMapShortcut", $_COOKIE["orion_user_session"])->fetch(PDO::FETCH_ASSOC);
 
-	# Build the position update form. 
-	# TODO this needs to have the existing choice selected!
-	if($userMapShortcut){ # $userMapShortcut[zoom]
-		$output .= "<form method=post><p>$userMapShortcut[name]'s home co-ordinates: 
-		<label>X: </label><input type=number name=x_pos min=-4000 max=4000 size=5 value=$userMapShortcut[x_pos] /> 
-		<label>Z: </label><input type=number name=z_pos min=-4000 max=4000 size=5 value=$userMapShortcut[z_pos] />
-		<label>Zoom: </label><select name=zoom  />
-		<option value=max>Max</option>
-		<option value=-1>-1</option>
-		<option value=-2>-2</option>
-		<option value=-3>-3</option>
-		<option value=-4>-4</option>
-		<option value=-5>-5</option>
-		</select>
-		<input type=submit name=update_map_shortcut value='Go' /> 
-		</p></form>";
-
-	}
-	else{
-		$output .= "<p class=error>Something weent wrong getting your map shortcut.</p>";
-	}
+	include_once("views/account.php");
 
 ?>
---
+++ views/account.php
@@ -0,0 +1,36 @@
+<?php
+	if($userData){
+		$output .= "<p>$userData->username</p>";
+		$output .= "<p>$userData->email</p>";
+		$output .= "<p><img src='$userData->mc_avatar' />$userData->mc_username</p>";
+	}
+	else{
+		$output .= "<p class=error>Something weent wrong getting your account details.</p>";
+	}
+
+
+	# Build the position update form. 
+	# TODO this needs to have the existing choice selected!
+	if($userMapShortcut){ # $userMapShortcut[zoom]
+		$output .= "<form method=post><p>$userMapShortcut[name]'s home co-ordinates: 
+		<label>X: </label><input type=number name=x_pos min=-4000 max=4000 size=5 value=$userMapShortcut[x_pos] /> 
+		<label>Z: </label><input type=number name=z_pos min=-4000 max=4000 size=5 value=$userMapShortcut[z_pos] />
+		<label>Zoom: </label><select name=zoom  />\n";
+		
+		$zoomLevels = ["Max", -1, -2, -3, -4, -5];
+		foreach($zoomLevels as $level){
+			if($level == $userMapShortcut["zoom"]) 
+				$output .= "<option value=$level selected>$level</option>";
+			else 
+				$output .= "<option value=$level>$level</option>";
+		}
+
+		$output .= "</select>
+		<input type=submit name=update_map_shortcut value='Go' /> 
+		</p></form>";
+
+	}
+	else{
+		$output .= "<p class=error>Something weent wrong getting your map shortcut.</p>";
+	}
+?>
