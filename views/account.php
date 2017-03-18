1069ff26c177ba17035a46478c42f1bce2f44f5a
AMGitsKriss <sp4m@k-jessop.co.uk>
2017-03-18 20:19:46 +0000

    Shortcut name is based on username form database, rather than cookie.

--- views/account.php
+++ views/account.php
@@ -11,28 +11,26 @@
 
 
 	# Build the position update form. 
-	# TODO this needs to have the existing choice selected!
-	if($userMapShortcut){ # $userMapShortcut[zoom]
-		$output .= "<form method=post><p>$userMapShortcut[name]'s home co-ordinates: 
-		<label>X: </label><input type=number name=x_pos min=-4000 max=4000 size=5 value=$userMapShortcut[x_pos] /> 
-		<label>Z: </label><input type=number name=z_pos min=-4000 max=4000 size=5 value=$userMapShortcut[z_pos] />
-		<label>Zoom: </label><select name=zoom  />\n";
-		
-		$zoomLevels = ["Max", -1, -2, -3, -4, -5];
-		foreach($zoomLevels as $level){
-			if($level == $userMapShortcut["zoom"]) 
-				$output .= "<option value=$level selected>$level</option>";
-			else 
-				$output .= "<option value=$level>$level</option>";
-		}
-
-		$output .= "</select>
-		<input type=submit name=update_map_shortcut value='Go' /> 
-		</p></form>";
-
+	# If the $userMapShortcut variable is a string, it's not in the list.
+	if(gettype($userMapShortcut) == "string"){ # $userMapShortcut[zoom]
+		$userMapShortcut = ["name"=>"", "x_pos"=>"", "z_pos"=>"", "zoom"=>""]
 	}
-	else{
-		$output .= "<p class=error>Something weent wrong getting your map shortcut.</p>";
+	$output .= "<form method=post><p>$userMapShortcut[name]'s home co-ordinates: 
+	<label>X: </label><input type=number name=x_pos min=-4000 max=4000 size=5 value=$userMapShortcut[x_pos] /> 
+	<label>Z: </label><input type=number name=z_pos min=-4000 max=4000 size=5 value=$userMapShortcut[z_pos] />
+	<label>Zoom: </label><select name=zoom  />\n";
+	
+	$zoomLevels = ["Max", -1, -2, -3, -4, -5];
+	foreach($zoomLevels as $level){
+		if($level == $userMapShortcut["zoom"]) 
+			$output .= "<option value=$level selected>$level</option>";
+		else 
+			$output .= "<option value=$level>$level</option>";
 	}
+
+	$output .= "</select>
+	<input type=submit name=update_map_shortcut value='Go' /> 
+	</p></form>";
+	
 	$output .= "</div></div>";
 ?>
