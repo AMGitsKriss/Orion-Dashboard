fe67ec1a1ee89af397e22ff030031560cc04b067
Kriss <sp4m@k-jessop.co.uk>
2017-03-19 23:56:21 +0000

    aggregator style tweaks

--- views/mylinks.php
+++ views/mylinks.php
@@ -7,11 +7,11 @@
 		//Only echo out the control options on entries that are the user's. I.E. Not public.
 		//If logged in and entry owner is the user, OR the user is level 2 or higher...
 		$output .= "<div class=link-container>
-		<div class=link-date>$added</div>
+		<div class=link-date><i class='fa fa-calendar' aria-hidden=true title='$added'></i></div>
 		<div class=link-hash><a href='$host/$row[hash]'>$row[hash]</a></div>
 		<div class=link-name>$linkName</div>
 		<div class=link-link><a href='$row[url]' target='_blank'>$row[url]</a></div>
-		<div class=link-controls>[&#9881;]<a href='#' onclick='deleteEntry(\"delete_entry\", $row[id] )'>[&#10060;]</a></div>
+		<div class=link-controls><i class='fa fa-cog' aria-hidden=true></i> <a href='#' onclick='deleteEntry(\"delete_entry\", $row[id] )'><i class='fa fa-times' aria-hidden=true></i></a></div>
 		</div>\n";
 	}
 	$output .= "</div>";
