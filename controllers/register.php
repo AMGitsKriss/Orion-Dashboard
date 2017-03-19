aeea368038d63a110bbc9ecc365d0a6dffd39407
Kriss <sp4m@k-jessop.co.uk>
2017-03-19 22:17:36 +0000

    letters and numbers only in usernames

--- controllers/register.php
+++ controllers/register.php
@@ -15,8 +15,12 @@
 		$loginCheck = new KUserManager($database);
 		$signin_error = "<p class=error>Yeah... That doesn't work yet.</p>";
 
+		# If there's invalid cahracters in the username...
+		if(preg_match('/[^A-Za-z0-9]/', $_POST['username'])){
+			$signin_error = "<p class=error>Forbidden characters in Username.</p>";
+		}
 		# If the password's match...
-		if($_POST["password1"] === $_POST['password2']){
+		else if($_POST["password1"] === $_POST['password2']){
 			$status = $loginCheck->makeUser($_POST['username'], $_POST['email'], $_POST['password1']);
 			if(gettype($status) == "string"){
 				# Print the error
