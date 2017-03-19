c3c149f13c15c8b545263534149f45e1db4a0266
Kriss <sp4m@k-jessop.co.uk>
2017-03-19 22:10:46 +0000

    register form all friends required

--- views/register.php
+++ views/register.php
@@ -2,11 +2,11 @@
 	if(!isset($_COOKIE["orion_user_session"])){
 		$output.="<div class='signin-form'>
 		<form method=post action=''>
-		<p><input type=text name='username' placeholder='Username' /></p>
-		<p><input type=text name='email' placeholder='Email' /></p>
-		<p><input type=password name='password1' placeholder='Password' /></p>
-		<p><input type=password name='password2' placeholder='Repeat Password' /></p>
-		<p><input type=submit name='register-form' value='Register'/></p>
+		<p><input type=text name='username' placeholder='Username' required /></p>
+		<p><input type=email name='email' placeholder='Email' required /></p>
+		<p><input type=password name='password1' placeholder='Password' required /></p>
+		<p><input type=password name='password2' placeholder='Repeat Password' required /></p>
+		<p><input type=submit name='register-form' value='Register' required /></p>
 		$signin_error
 		</form>
 		</div>";
