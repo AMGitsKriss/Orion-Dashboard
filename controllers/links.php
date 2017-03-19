052c0c77d18d4027d5e2210d0d247a1811996cb1
Kriss <sp4m@k-jessop.co.uk>
2017-03-19 22:57:44 +0000

    Imported Aggregator display into dashboard

---
+++ controllers/links.php
@@ -0,0 +1,22 @@
+<?php
+
+	include_once("views/header.php");
+	$output .= "<section>";
+
+	$output .= "<h2>Aggregator</h2>";
+
+	$sql = null;
+
+	//Logged in as... 
+	#include("views/heading.php");
+
+	//Parsing the webpage
+	require("models/KHTMLParser.class.php");
+	$parser = new KHTMLParser();
+
+	//loading the lists, depending on user elevation
+	$result=$database->query("selectList", $_COOKIE["orion_user_session"]);
+
+	include("views/posts.php");
+	
+?> 
