8d46f1aab41c1e32f72605363eebc2e41344de9e
Kriss <sp4m@k-jessop.co.uk>
2017-03-28 00:43:55 +0100

    "Who's online" TS3 caching

--- views/players.php
+++ views/players.php
@@ -1,7 +1,7 @@
 <?php
 	
 	//Players table
-	$output .= "<table class=players-table table-bordered table-striped>\n<thead>\n<tr>\n<th>Players</th>\n</tr>\n</thead>\n<tbody>\n";
+	$output .= "<table class=players-table table-bordered table-striped>\n<thead>\n<tr>\n<th>Minecraft</th>\n</tr>\n</thead>\n<tbody>\n";
 
 	foreach( $online_players as $player ){
 		$output .= "<tr>\n<td>\n<img src='$player[1]' />$player[0]</td>\n</tr>\n";
--- views/teamspeak.php
+++ views/teamspeak.php
@@ -2,20 +2,8 @@
 
 	# View stuff goes in here
 
-	$output .= "<table class=players-table table-bordered table-striped>\n<thead>\n<tr>\n<th>Players</th>\n</tr>\n</thead>\n<tbody>\n";
+	$output .= file_get_contents("views/templates/cache_ts3.html");
 
-	foreach($arr_ChannelList as $ts3_Channel){
-		$room = $ts3_VirtualServer->channelGetByName("$ts3_Channel");
-		$output .= "<tr>\n<td>$room</td>\n</tr>\n";
-		foreach($room as $user){
-			$userString = "".$user;
-			if(!(strpos($userString, 'serveradmin') !== false)){
-				$output .= "<tr>\n<td>&nbsp;-&nbsp;&nbsp;$userString</td>\n</tr>\n";
-			}
-		}
-	}
-
-	$output .= "</tbody>\n</table>";	
 	# Iterate over ChannelList to get the list of channels.
 	# Iterate over ClientList to get clients and their occupied channels.
 	# Merge the things together.
