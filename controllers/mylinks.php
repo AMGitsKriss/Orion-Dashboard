a383752b6bdd91c86c3ad1fc0fcac2281e771eb8
AMGitsKriss <sp4m@k-jessop.co.uk>
2017-03-27 02:53:34 +0100

    Link posting & deleting

--- controllers/mylinks.php
+++ controllers/mylinks.php
@@ -1,19 +1,33 @@
 <?php
 
 	include_once("views/header.php");
+	$output .= "<script type='text/javascript' src='scripts/functions.js'></script>\n";
 	$output .= "<section>";
 
 	$output .= "<h2>Aggregator</h2>";
 
-
-	$postLinkMsg = "";
-	if(isset($_POST['newlink'])){
-		$postLinkMsg = "<p>POST REQUEST SUCCESSFUL</p>";
-	}
-
 	//Parsing the webpage
 	require("models/KHTMLParser.class.php");
 	$parser = new KHTMLParser();
+	$postLinkMsg = "";
+
+	if(isset($_POST['delete_entry'])){
+		//TODO - Delete that entry.
+		$database->query("deletePost",$_POST['delete_entry'], $_COOKIE['orion_user_session']);
+		$postLinkMsg = "<p>Link successfully deleted.</p>";
+	}
+
+	# If we're here with the query sting, and not another page, and the parser triggers successfully.
+	if($_SERVER['QUERY_STRING'] != "" && !isset($_GET['link']) && !isset($_GET['page']) && $parser->handle($_SERVER['QUERY_STRING'], $database)){
+		header("Location: $host/links");
+	}
+
+	else if(isset($_POST['newlink']) && $parser->handle($_POST['url'], $database)){
+		header("Location: $host/links");
+	}
+	else if($page != "links") {
+		$postLinkMsg = "<p>Error: Something went wrong adding that entry.</p>";
+	}
 
 	//loading the lists, depending on user elevation
 	$result=$database->query("selectList", $_COOKIE["orion_user_session"]);
--- index.php
+++ index.php
@@ -11,12 +11,26 @@
 		setcookie("orion_user_session", $_COOKIE["orion_user_session"], time() + (86400 * 30), "/"); // 86400 = 1 day
 	}
 
+	## 
+	#	The Main logic block. 
+	#	Determine if we're handling a shortened url or a page. If neither, it must be a URL for the aggregator.
+	##
+
 	# Handling link codes.
 	if(isset($_GET['link'])) include_once("controllers/shorturl.php");
 
-	#Page navigator
+	# Page navigator
 	$page = "";
-	if(isset($_GET['page'])) $page = $_GET['page'];
+	if(isset($_GET['page'])){
+		$page = $_GET['page'];
+	}
+
+	# Aggregator
+	if($_SERVER['QUERY_STRING'] != "" && !isset($_GET['link']) && !isset($_GET['page'])){
+		$page = "links";
+		// Note: This will take ANY server query, so it has to go after every other query.
+	}
+
 	switch ($page){
 		case "account":
 			# If logged in, go to account. Else, skip to login page.
@@ -50,6 +64,7 @@
 			include_once("controllers/home.php");
 			break;
 	}
+
 
  
 	$output .= "</section></body></html>";
