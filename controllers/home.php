125ece28b0c028910ce4ec2af0cc0e6045329516
Kriss <sp4m@k-jessop.co.uk>
2017-03-28 00:43:55 +0100

    "Who's online" TS3 caching

--- controllers/home.php
+++ controllers/home.php
@@ -46,13 +46,17 @@
 	# Online Micnraft players
 	$online_players = getUserList($host);
 
-	# Teamspeak people online...
-	require_once("libraries/TeamSpeak3/TeamSpeak3.php");
-	# Connect to the server as admin
-	$ts3_VirtualServer = TeamSpeak3::factory("serverquery://$ts_username:$ts_password@192.168.0.23:10011/?server_port=9987");
-	// query clientlist from virtual server
-	$arr_ClientList = $ts3_VirtualServer->clientList();
-	$arr_ChannelList = $ts3_VirtualServer->channelList();
+
+	// If the ts3 online-users cache file is more than a minute old, we'll regenerate it before displaying it.
+	if(time() - filemtime("views/templates/cache_ts3.html") > 60){
+		try{
+			updateTS3Cache($ts_username, $ts_password);
+		}
+		// We've flooded the query system and timed it out.
+		catch (TeamSpeak3_Adapter_ServerQuery_Exception $e) {
+
+		}
+	}
 
 	include_once("views/sidebar.php");
 
@@ -64,4 +68,35 @@
 
 	#Bulk of page
 	$output .= "\n</div>";
+
+	function updateTS3Cache($ts_username, $ts_password){
+		# Teamspeak people online...
+		require_once("libraries/TeamSpeak3/TeamSpeak3.php");
+		# Connect to the server as admin
+		$ts3_VirtualServer = TeamSpeak3::factory("serverquery://$ts_username:$ts_password@192.168.0.23:10011/?server_port=9987");
+	
+		//Cache file location
+		$ts3_cache_dir = "views/templates/cache_ts3.html";
+
+		//Open the file for writing.
+		$cache_file = fopen($ts3_cache_dir, "w");
+
+		//Declare table
+		fwrite($cache_file, "<table class=players-table table-bordered table-striped>\n<thead>\n<tr>\n<th title='&plusmn;1 min'>Teamspeak</th>\n</tr>\n</thead>\n<tbody>\n");
+
+		// query channelist from virtual server
+		foreach($ts3_VirtualServer->channelList() as $ts3_Channel){
+			//Iterate over the list and get the users in each room
+			$room = $ts3_VirtualServer->channelGetByName("$ts3_Channel");
+			fwrite($cache_file, "<tr>\n<td>$room</td>\n</tr>\n");
+			foreach($room as $user){
+				$userString = "".$user;
+				if(!(strpos($userString, 'serveradmin') !== false)){
+					fwrite($cache_file, "<tr>\n<td>&nbsp;-&nbsp;&nbsp;$userString</td>\n</tr>\n");
+				}
+			}
+		}
+		// Close table
+		fwrite($cache_file, "</tbody>\n</table>");	
+	}
 ?>
