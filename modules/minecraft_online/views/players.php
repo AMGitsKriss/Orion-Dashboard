0cd9954daae4aa328f00acd5916eeedd637f4fc7
Kriss <sp4m@k-jessop.co.uk>
2017-10-03 17:14:41 +0100

    Copied core code from main application to the module. (Issue #34)

---
+++ modules/minecraft_online/views/players.php
@@ -0,0 +1,10 @@
+<?php	
+	//Players table
+	$output .= "<table class=players-table table-bordered table-striped>\n<thead>\n<tr>\n<th>Minecraft</th>\n</tr>\n</thead>\n<tbody>\n";
+
+	foreach( $online_players as $player ){
+		$output .= "<tr>\n<td>\n<img src='$player[1]' />$player[0]</td>\n</tr>\n";
+	}
+
+	$output .= "</tbody>\n</table>";	
+?>
