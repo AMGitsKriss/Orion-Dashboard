2fa6c86dd6288bb5be3f20ae8c60a9288e855fef
Kriss <sp4m@k-jessop.co.uk>
2017-10-03 13:23:17 +0100

    Constrain overviewer shortcuts to -4000 and 4000

--- controllers/account.php
+++ controllers/account.php
@@ -9,7 +9,17 @@
 	if(isset($_POST['update_map_shortcut'])){
 		# There was a fancy statement for this...
 		# Statement takes 8 variables for 4 args. This is the easiest way of doing it...
-		$database->query("updateMapShortcut", $username, $_POST['x_pos'], $_POST['z_pos'], $_POST['zoom'], $username, $_POST['x_pos'], $_POST['z_pos'], $_POST['zoom']);
+
+		$xPos = $_POST['x_pos'];
+		$zPos = $_POST['z_pos'];
+		$zoom = $_POST['zoom'];
+
+		//Verify that these values fit the world contstraints. It's a hack, but if it's outside the range, we'll just set it to 0.
+		$xPos = ($xPos < 4000 || $xPos > -4000) ? $xPos : 0;
+		$zPos = ($zPos < 4000 || $zPos > -4000) ? $zPos : 0;
+		$zoom = ($zoom < -5 || $zoom > -1) ? $zPos : -3;
+
+		$database->query("updateMapShortcut", $username, $xPos, $zPos, $zoom, $username, $xPos, $zPos, $zoom);
 		$output .= "<p>Your Overviewer co-ordinates were updated.</p>";
 	}
 
