ff9a14cbefbb3800ff0cf69ce03ef6dd9d64a395
Kriss <sp4m@k-jessop.co.uk>
2017-11-11 20:29:36 +0000

    Refactored more views to be inline html.

--- controllers/commoncontroller.php
+++ controllers/commoncontroller.php
@@ -4,23 +4,21 @@
 	/*
 	 *	where each row: [0] = Name, [1] = URL, [2] = Icon
 	 */
-	$links = [["Home", $host, '<i class="fa fa-home" aria-hidden="true"></i>'],
-		["Minecraft Map", $host."/map", '<i class="fa fa-globe" aria-hidden="true"></i>']];
+	$links = [['<i class="fa fa-home" aria-hidden="true"></i> Home', $host],
+		['<i class="fa fa-globe" aria-hidden="true"></i> Minecraft Map', $host."/map"]];
 
-	$controls = [["", $host."/login", '<i class="fa fa-sign-in" aria-hidden="true"></i>'],
-		["Repo", "https://github.com/AMGitsKriss/Orion-Dashboard' target='_blank", '<i class="fa fa-github" aria-hidden="true"></i>']];
+	$controls = [['<i class="fa fa-sign-in" aria-hidden="true"></i>', $host."/login"],
+		['<i class="fa fa-github" aria-hidden="true"></i> Repo', "https://github.com/AMGitsKriss/Orion-Dashboard' target='_blank"]];
 
 	$mapShortcuts = $database->getByOrder("map_shortcuts");
 
 	// TODO - This is a bodge for the time being.
 	if(isset($_COOKIE["orion_user_session"])) {
-		$controls[0] = ["", $host."/logout", '<i class="fa fa-sign-out" aria-hidden="true"></i>'];
-		$controls[1] = ["", $host."/account", '<i class="fa fa-user-circle-o" aria-hidden="true"></i>'];
-		$controls[2] = ["Repo", "https://github.com/AMGitsKriss/Orion-Dashboard' target='_blank", '<i class="fa fa-github" aria-hidden="true"></i>'];
+		$controls = [['<i class="fa fa-sign-out" aria-hidden="true"></i>', $host."/logout"], ['<i class="fa fa-user-circle-o" aria-hidden="true"></i>', $host."/account"], ['<i class="fa fa-github" aria-hidden="true"></i> Repo', "https://github.com/AMGitsKriss/Orion-Dashboard' target='_blank"]];
 
-		$links[3] = ["Projects", "$host/projects", '<i class="fa fa-list-ul" aria-hidden="true"></i>'];
-		$links[2] = ["My Links", "$host/links", '<i class="fa fa-bookmark" aria-hidden="true"></i>'];
+		$links[3] = ['<i class="fa fa-list-ul" aria-hidden="true"></i> Projects', "$host/projects"];
+		$links[2] = ['<i class="fa fa-bookmark" aria-hidden="true"></i> My Links', "$host/links"];
 	}
 
-	require_once("views/header.php");
+	require_once("views/headerview.php"); 
 ?>
