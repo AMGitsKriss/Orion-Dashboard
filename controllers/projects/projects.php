ae52b251e4651ac990baa5dcd770ab03e4d3bddb
Kriss <sp4m@k-jessop.co.uk>
2017-11-16 21:13:24 +0000

    Basics of Projects page

--- controllers/projects/projects.php
+++ controllers/projects/projects.php
@@ -1,10 +1,15 @@
 <?php
 
 	include_once("controllers/commoncontroller.php");
-	
+
 	$viewData->page_title = "Active Issues:";
 	$viewData->sidebar[0] = [["A thing."]];
-	$viewData->content = ["There's nothing here... Yet."];
+
+	$row = new stdClass();
+	$row->id = "AB-1264";
+	$row->name = "A thing";
+
+	$viewData->content[0] = $row;
 
 	include_once("views/sidebarview.php");
 	include_once("views/projects/projectview.php");
